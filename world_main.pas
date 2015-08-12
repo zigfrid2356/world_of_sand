11fd1cc15f25ccb61d32b3e5a4d8aa34a19f322b
Zigfrid2356 <zigfridone@gmail.com>
2015-08-12 21:32:46 +0500

    first commit

--- world_main.pas
+++ world_main.pas
@@ -20,69 +20,322 @@
    
    
 }
-
+{v.0.002pa}{12.08.2015}
 
 program world_of_sand;
 
-uses crt;
+uses crt,sysutils;
 
 type
 body =record
+name:string;
 hp,mp,exp,lvl:integer;
+//+10.08.2015
+dmg,ign_dmg,veapon,armor,attak,defense:integer;
+gold:integer;
+end;
+erath =record
+x,y:byte;
+structure:char;
 end;
 var
-
+map:array[0..255,0..255] of erath;
 hero:body;
-menu_key:byte;
-procedure hero_output(i:byte);
+monster:body;
+menu_key:char;
+i,j,n,m:integer;//Ã¡Ã§Ã±Ã¢Ã§Â¨ÂªÂ¨{ÑÑ‡Ñ‘Ñ‚Ñ‡Ð¸ÐºÐ¸}
+lang: text;
+monster_name:text;
+text:array[0..100] of string;
+text_name:array[0..100] of string;
+hero_save:file of body;
+map_save:file of erath;
+
+simbol: array [0..4] of char;
+function name_generate(command:string):string;//+12.08.2015
+var
+s:string;
+begin
+s:='monster_'+command+'.name';
+
+assign(monster_name,s);
+reset(monster_name);
+m:=1;
+while not eof(monster_name) do begin//1.1
+readln(monster_name,text_name[m]);
+m:=m+1;
+end;//1.1
+close(monster_name);
+name_generate:=text_name[random(m)];
+end;
+procedure hero_generate(h:string);//+12.08.2015
+begin
+if h='hero' then begin //1
+hero.name:='@sub_name';
+hero.lvl:=1;
+
+hero.hp:=10*hero.lvl;
+hero.mp:=10*hero.lvl;
+hero.exp:=0;
+
+
+hero.veapon:=3;
+hero.armor:=3;
+hero.attak:=1;
+hero.defense:=1;
+hero.dmg:=hero.veapon*hero.attak;
+hero.ign_dmg:=hero.armor*hero.defense;
+hero.gold:=1;
+end;//1
+if h='monster_human' then begin//2
+monster.name:=name_generate('human');
+monster.lvl:=(hero.lvl-1)+random(3);
+
+monster.hp:=5*monster.lvl;
+monster.mp:=5*monster.lvl;
+monster.exp:=monster.lvl+random(monster.lvl);
+
+
+monster.veapon:=1+random(monster.lvl);
+monster.armor:=1+random(monster.lvl);
+monster.attak:=1;
+monster.defense:=1;
+monster.dmg:=monster.veapon*monster.attak;
+monster.ign_dmg:=monster.armor*monster.defense;
+monster.gold:=monster.lvl+random(monster.lvl);
+end;//2
+if h='monster_beast' then begin //3
+monster.name:=name_generate('beast');
+monster.lvl:=(hero.lvl-1)+random(3);
+
+monster.hp:=5*monster.lvl;
+monster.mp:=5*monster.lvl;
+monster.exp:=monster.lvl+random(monster.lvl)+5;
+
+
+monster.veapon:=1;
+monster.armor:=1;
+monster.attak:=1;
+monster.defense:=1;
+monster.dmg:=monster.veapon*monster.attak;
+monster.ign_dmg:=monster.armor*monster.defense;
+monster.gold:=0;
+end;//3
+end;
+procedure save;
+begin
+assign(hero_save,'hero.save');
+rewrite(hero_save);
+write(hero_save,hero);
+close(hero_save);
+
+assign(map_save,'map.save');
+rewrite(map_save);
+for i:=0 to 255 do begin//1.1
+	for j:=0 to 255 do begin//1.2
+write(map_save,map[i,j]);
+end;end;//1.1//1.2
+close(map_save);
+end;
+procedure load;
+begin
+assign(hero_save,'hero.save');
+reset(hero_save);
+read(hero_save,hero);
+close(hero_save);
+
+assign(map_save,'map.save');
+reset(map_save);
+for i:=0 to 255 do begin//1.1
+	for j:=0 to 255 do begin//1.2
+read(map_save,map[i,j]);
+end;end;//1.1//1.2
+close(map_save);
+end;
+procedure lvlup;
+begin
+
+if hero.exp>=hero.lvl*5 then begin//1
+hero.exp:=hero.exp-hero.lvl*5;
+hero.lvl:=hero.lvl+1;
+
+hero.hp:=10*hero.lvl;
+hero.mp:=10*hero.lvl;
+
+hero.veapon:=3;
+hero.armor:=3;
+hero.attak:=1;
+hero.defense:=1;
+hero.dmg:=hero.veapon*hero.attak;
+hero.ign_dmg:=hero.armor*hero.defense;
+end;//1
+end;
+procedure battle;
+begin
+hero_generate('monster_beast');
+i:=1;
+repeat begin//1
+clrscr;
+
+writeln(text[14],': ',i);
+writeln('-------------------------');
+writeln('|',hero.name,'                    ', monster.name,'|');
+writeln('|',text[5],'    ', text[5],'|');
+writeln('|',hero.hp,'                    ', monster.hp,'|');
+writeln('|',text[6],'          ', text[6],'|');
+writeln('|',hero.mp,'                   ', monster.mp,'|');
+writeln('|',text[12],'          ', text[12],'|');
+writeln('|',hero.dmg ,'                    ',monster.dmg,'|');
+writeln('-------------------------');
+//writeln(text[13],hero.ign_dmg,'    ',monster.ign_dmg );
+writeln(text[7],hero.exp );
+writeln(text[8],hero.lvl );
+i:=i+1;
+hero.hp:=hero.hp-abs(monster.dmg-hero.ign_dmg);
+monster.hp:=monster.hp-abs(hero.dmg-monster.ign_dmg);
+
+delay(500);
+end;//1
+until (hero.hp<=0) or(monster.hp<=0);
+if hero.hp<=0 then exit;
+if monster.hp<=0 then begin//2
+hero.gold:=hero.gold+monster.gold;
+hero.exp:=hero.exp+monster.exp;
+lvlup;
+
+writeln(text[15]);
+readln();
+
+end;//2 
+end;
+procedure hero_output;
 begin
 clrscr;
-writeln('®çª¨ ¦¨§­¨ ',hero.hp );
-writeln('®çª¨ ¬ ­ë ',hero.mp );
-writeln('®çª¨ ®¯ëâ  ',hero.exp );
-writeln('ãà®¢¥­ì ',hero.lvl );
+writeln(text[5],' ',hero.hp );
+writeln(text[6],hero.mp );
+writeln(text[7],hero.exp );
+writeln(text[8],hero.lvl );
+writeln(text[12],hero.dmg );
+writeln(text[13],hero.ign_dmg );
 readln();
 end;
 
-procedure main_menu(i:byte);
+
+procedure map_output(x,y:byte);
+begin
+repeat begin//2.0
+clrscr;
+
+for i:=x-5 to x+5 do begin//2.1
+ for j:=y-5 to y+5 do begin//2.2
+ write(map[i,j].structure);
+ end;//2.2
+ writeln('');
+end;//2.1
+writeln(x,' ',y);
+writeln	('1- ->');
+writeln	('2- <-');
+writeln	('3- /\');
+writeln	('4- \/');
+writeln	('5- ',text[2]);
+//readln(menu_key);
+menu_key:=readkey;
+end;//2.0
+
+case menu_key of//3.0
+'1':begin//3.1 
+x:=x;y:=y+1; 
+map_output(x,y);
+end;//3.1
+'2':begin//3.2
+x:=x;y:=y-1; 
+map_output(x,y);
+ end;//3.2
+'3':begin//3.3
+x:=x-1;y:=y; 
+map_output(x,y);
+ end;//3.3
+'4':begin//3.4
+x:=x+1;y:=y;  
+map_output(x,y);
+ end;//3.4
+end;//3.0
+until menu_key='5';
+end;
+
+procedure main_menu;
 begin
 
 repeat begin//1
 ClrScr;
-writeln	('1- £¥à®©');
-writeln	('2- ª àâ ');
-writeln	('3- ¨­¢¥­â àì');
-writeln	('4- ¢ëå®¤');
-readln(menu_key);
+writeln	('1- ',text[10]);
+writeln	('2- ',text[9]);
+writeln	('3- ',text[11]);
+writeln	('4- ',text[17]);
+writeln	('5- ',text[2]);
+//readln(menu_key);
+menu_key:=readkey;
 case menu_key of
-1: begin hero_output(1);
- 
-	end;
-2:begin end;
-3:begin end;
+'1': begin //1.1
+	hero_output;
+	end;//1.1
+'2':	begin//1.2
+	map_output(128,128); main_menu;
+	end;//1.2
+'3':begin //1.3
+battle;
+end;//1.3
+'4': begin//1.4
+save;
+end//1.4
 end;
 end;//2 
-until menu_key=4;
+until menu_key='5';
 end;
 
 BEGIN
+i:=0;
+assign(lang,'text.lang');
+reset(lang);
+while not eof(lang) do begin
+readln(lang,text[i]);
+delete(text[i],1,3);
+i:=i+1;
+end;
+close(lang);
 
-writeln	('Œˆ …‘Š€');
+writeln	(text[1]);
 readln();
  ClrScr;
-writeln	('Œ…ž');
-writeln	('1- ­ ç âì');
-writeln	('2- ¢ë©â¨');
-readln(menu_key);
+writeln	(text[3]);
+writeln	('1-',text[4]);
+writeln	('2-',text[16]);
+writeln	('3-',text[2]);
+//readln(menu_key);
+menu_key:=readkey;
 case menu_key of
-1: begin 
-hero.hp:=10;
-hero.mp:=10;
-hero.exp:=0;
-hero.lvl:=1;
-main_menu(1); 
+'1': begin 
+//Ã¢Â¥Ã¡Ã¢Â®Â¢Â Ã¯ Â£Â¥Â­Â¥Ã Â Ã¦Â¨Ã¯ Â£Â¥Ã Â®Ã¯ Ð³ÐµÐ½ÐµÑ€Ð°Ñ†Ð¸Ñ Ð¿ÐµÑ€ÑÐ¾Ð½Ð°Ð¶Ð° {~ 12.08.2015}
+hero_generate('hero');
+//Ã¢Â¥Ã¡Ã¢Â®Â¢Â Ã¯ Â£Â¥Â­Â¥Ã Â Ã¦Â¨Ã¯ ÂªÂ Ã Ã¢Ã« {Ð³ÐµÐ½ÐµÑ€Ð°Ñ†Ð¸Ñ ÐºÐ°Ñ€Ñ‚Ñ‹ }
+simbol[0]:='.';
+simbol[1]:='.';
+simbol[2]:='#';
+simbol[3]:='|';
+simbol[4]:='.';
+for i:=0 to 255 do begin//1.1
+	for j:=0 to 255 do begin//1.2
+	map[i,j].x:=i;
+	map[i,j].y:=j;
+	map[i,j].structure:=simbol[random(5)];
+	end;//1.2
+end;//1.1
+
+
+main_menu; 
 end;
-2: exit
+'2':begin load; main_menu; end;
+'3': exit
 end;
 END.
 

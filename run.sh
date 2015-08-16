fca03273395d3136baef12ba536fbb4a9aad8f49
zigfrid2356 <zigfridone@gmail.com>
2015-08-16 14:11:19 +0500

    first commit

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 
-#!/bin/bash git init
-git add -A
+#!/bin/bash 
+git init
+git add .
 git commit -a -m 'first commit'
 git remote add origin git@github.com:username/reponame.git
 git push -u origin master

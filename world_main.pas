ad844b95d1f5e58a350b0b3d8d9690198b1a2dc2
Alexandr <zigfridone@gmail.com>
2016-01-11 23:09:54 +0800

    торговля

--- world_main.pas
+++ world_main.pas
@@ -763,9 +763,9 @@ clrscr;
 if command= 'trade' then tr:=text[69];
 if command= 'cell' then tr:=text[68];
 writeln('--------------------------------------------------------');
-writeln('|'+text[91]+'         |'+text[110]+'|'+text[12]+'|'+text[92]+'|'+text[102]+'|'+text[93]+'|');
+writeln('|'+name_tab(text[91],35)+'|'+name_tab(text[110],11)+'|'+name_tab(text[12],4)+'|'+text[92]+'|'+text[102]+'|'+text[93]+'|');
 for toi:=0 to 9 do begin//1
-writeln('|'+name_tab(t_o.bag[toi].name,17)+'|'+name_tab(item_info(t_o.bag[toi].tip),11)+'|'+name_tab(inttostr(t_o.bag[toi].base_dmg),4));
+writeln('|'+name_tab(t_o.bag[toi].name,35)+'|'+name_tab(item_info(t_o.bag[toi].tip),11)+'|'+name_tab(inttostr(t_o.bag[toi].base_dmg),4));
 end;//1
 readln();
 end;

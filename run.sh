b501c822036b93f8abd7f3fc47e1a36463bfe533
zigfrid2356 <zigfridone@gmail.com>
2016-02-18 17:38:11 +0300

    mdate

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 
 #!/bin/bash 
+mdate=" $(date +%d-%m-%Y\ %H:%M:%S) "
 git pull
 git init
 git add .
-git commit -a -m "(date +%d-%m-%Y\ %H:%M:%S)"
+git commit -a -m mdate
 git remote add origin git@github.com:username/reponame.git
 git push -u origin master

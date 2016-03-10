b50ada05d3d0e145497df8861a148882ff8a69c2
zigfrid2356 <zigfridone@gmail.com>
2016-03-10 19:13:27 +0400

    профессии

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 git pull
 git init
 git add .
-git commit -a -m 'авто одевалка мобов'
+git commit -a -m 'профессии'
 git remote add origin git@github.com:username/reponame.git
 git push -u origin master
